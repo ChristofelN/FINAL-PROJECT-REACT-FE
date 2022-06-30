c427f2a44f87cb16f70256cdad107ea7f68b68f8
Cristofel Nau <s11820005@student.unklab.ac.id>
2022-06-30 20:46:04 +0800

    Menambahkan gambar wisata pantai

--- src/HomePage/contentwrapper/ribboncontent/i1/index.js
+++ src/HomePage/contentwrapper/ribboncontent/i1/index.js
@@ -2,7 +2,7 @@ export default function Ribboncontent()
 {
     return(
         <div className="l-box-lrg is-center pure-u-1 pure-u-md-1-2 pure-u-lg-2-5">
-        <img width={300} alt="File Icons" className="pure-img-responsive" src="https://purecss.io/img/common/file-icons.png" />
+        <img width={300} alt="File Icons" className="pure-img-responsive" src="https://www.archipelagos.id/media/2021/05/Pantai-Likupang-2-rescale-min-800x533.jpg" />
       </div>
     )
 }
